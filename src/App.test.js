702067a9b18319e7fb0aa9e045371333e97c5bdd
Dejan22 <dejan.tomasevic@protonmail.com>
2018-01-24 12:58:27 +0100

    routes in src finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App'
+import BatchesContainer from './batches/BatchesContainer'
+
+
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('wraps everything in a div tag', () => {
+    expect(app).to.have.tagName('div')
+  })
+
+  it('contains the BatchesContainer', () => {
+    expect(app).to.have.descendants(BatchesContainer)
+  })
+})
